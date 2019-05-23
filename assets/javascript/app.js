68f615d9ec8d5f92ca113b6166117d43ab85870a
cariasp27 <paul.carias@yahoo.com>
2019-05-23 11:16:55 -0400

    portfolio overhaul

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
-$(".card").on("click", function(){
-    let link = $(this).attr("data-link")
-});
+// LINKEDIN HOVER ANIMATION
+$("#lnkd").on("mouseenter", function() {
+    TweenMax.to(this, 0.4, {scale:1.4, ease:Bounce.easeOut})
+    TweenMax.to(this, 0.2, {scale:1, delay:0.4})
+  })
+  // PICTURE HOVER ANIMATION
+  $(".pict").on("mouseenter", function() {
+    TweenMax.to(this, 0.4, {scale:1.1, ease:Bounce.easeOut})
+    TweenMax.to(this, 0.2, {scale:1, delay:0.4})
+  })
+
+  
